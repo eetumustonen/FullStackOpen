c428d6db0795863d4abcd8cf26e17f7ffe5b9f2f
Eetu <eetu.mustonen998@gmail.com>
2022-12-27 18:52:40 +0200

    small change in kurssitiedot

--- osa2/kurssitiedot/src/components/course.js
+++ osa2/kurssitiedot/src/components/course.js
@@ -19,9 +19,11 @@ const Part = ({ part }) => {
 const Total = ({ parts }) => {
   return (
       <p>
-        Number of exercises {
-          parts.reduce((sum, part) => sum + part.exercises, 0)
-        }
+        <strong>
+            Total exercises {
+                parts.reduce((sum, part) => sum + part.exercises, 0)
+            }
+        </strong>
       </p>
   )
 }
